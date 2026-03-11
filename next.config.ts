f6cf2ba94ac4b64f293ab451a96319294870f65a
lucasgontijo13 <lucasgontijo111@gmail.com>
2026-03-11 13:07:38 -0300

    config para deploy

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Isso permite que o deploy termine mesmo se houver erros de tipagem
     ignoreBuildErrors: true, 
-  },
-  eslint: {
-    // Isso ignora avisos de formatação durante o build
-    ignoreDuringBuilds: true,
-  },
+  }
 };
 
 export default nextConfig;
